d364696756ac4658340251e4977e427b2f4c7879
sangTran <sangthien2010@gmail.com>
2020-04-28 11:20:36 -0500

    adding session and content security policy

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-var winston = require('winston');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+var winston = require("winston");
 
-const bodyParser = require('body-parser');
-const app = express()
-app.use(cors())
-app.use(helmet())
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => {
+  res.cookie("session", "1", { httpOnly: true });
+  res.cookie("session", "1", { secure: true });
+  res.set({
+    "Content-Security-Policy": "script-src 'self' 'https://apis.google.com'",
+  });
+  res.send("Hello World!");
+});
 
-app.post('/secret', (req, res) => {
+app.post("/secret", (req, res) => {
   const { userInput } = req.body;
   console.log(userInput);
   if (userInput) {
-    winston.log('info', 'user input: ' + userInput);
-    res.status(200).json('success');
+    winston.log("info", "user input: " + userInput);
+    res.status(200).json("success");
   } else {
-    winston.error('This guy is messing with us:' + userInput);
-    res.status(400).json('incorrect submission')
+    winston.error("This guy is messing with us:" + userInput);
+    res.status(400).json("incorrect submission");
   }
-})
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
